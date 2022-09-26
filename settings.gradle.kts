4544c0707fd9f8a369cd9ee5756779653fddefed
David <santamadavid@gmail.com>
2022-09-26 08:18:11 +0200

    Basic structure -empty project-

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CleanArchitectureKMM"
+include(":androidApp")
+include(":shared:viewmodels")
+include(":shared:usecases")
+include(":shared:entities")
+include(":shared:reposotories")
+include(":shared:datasources")
