b5c262843b6974ec6d58e8ed7fcad2fec08c8815
David <santamadavid@gmail.com>
2023-01-08 20:22:33 +0100

    add title and rating to NowPlayingContent

--- androidApp/src/main/java/com/santukis/cleanarchitecturekmm/android/home/components/HomeScreen.kt
+++ androidApp/src/main/java/com/santukis/cleanarchitecturekmm/android/home/components/HomeScreen.kt
@@ -4,8 +4,11 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Circle
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
@@ -16,8 +19,10 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.santukis.cleanarchitecturekmm.android.theme.MovieTheme
 import com.santukis.cleanarchitecturekmm.android.theme.WhiteTransparent
@@ -68,7 +73,7 @@ private fun NowPlayingContent(
             flingBehavior = rememberSnapperFlingBehavior(lazyListState = listState),
             modifier = modifier
                 .fillMaxWidth()
-                .height(height = LocalConfiguration.current.screenWidthDp.dp)
+                .height(height = (LocalConfiguration.current.screenHeightDp * 0.7).dp)
         ) {
             items(nowPlayingMoviesState.movies.size) { index ->
                 val movie = nowPlayingMoviesState.movies[index]
@@ -81,7 +86,7 @@ private fun NowPlayingContent(
                             .fillParentMaxWidth()
                             .drawWithCache {
                                 val gradient = Brush.verticalGradient(
-                                    colors = listOf(Color.Transparent, Color.Transparent, Color.Black),
+                                    colors = listOf(Color.Transparent, Color.Black),
                                     startY = 0f,
                                     endY = size.height
                                 )
@@ -96,12 +101,48 @@ private fun NowPlayingContent(
                         ,
                         contentScale = ContentScale.Crop
                     )
+
+                    Column(
+                        modifier = modifier
+                            .fillParentMaxWidth()
+                            .align(Alignment.BottomStart)
+                            .padding(vertical = 32.dp, horizontal = 8.dp),
+                        horizontalAlignment = Alignment.Start
+                    ) {
+                        Text(
+                            text = movie.titles.title,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 22.sp,
+                            color = Color.White,
+                            maxLines = 2,
+                            modifier = Modifier
+                                .padding(vertical = 4.dp)
+                        )
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Icon(
+                                modifier = Modifier
+                                    .size(16.dp),
+                                imageVector = Icons.Filled.Star,
+                                contentDescription = "",
+                                tint = MaterialTheme.colors.secondary
+                            )
+                            Text(
+                                modifier = Modifier
+                                    .padding(horizontal = 8.dp),
+                                text = movie.rating.getText(),
+                                fontSize = 12.sp,
+                                color = Color.White
+                            )
+                        }
+                    }
                 }
             }
         }
 
         LazyRow(
-            modifier =  Modifier
+            modifier = Modifier
                 .align(Alignment.BottomCenter)
                 .padding(8.dp)
         ) {
@@ -110,7 +151,7 @@ private fun NowPlayingContent(
 
                 Icon(
                     imageVector = Icons.Filled.Circle,
-                    contentDescription = "selected Image",
+                    contentDescription = "",
                     modifier = Modifier
                         .padding(horizontal = 2.dp)
                         .width(8.dp)
--- shared/entities/src/commonMain/kotlin/com/santukis/entities/movies/Rating.kt
+++ shared/entities/src/commonMain/kotlin/com/santukis/entities/movies/Rating.kt
@@ -3,4 +3,7 @@ package com.santukis.entities.movies
 data class Rating(
     val average: Double,
     val count: Int
-)
+) {
+
+    fun getText(): String = "$average ($count)"
+}
