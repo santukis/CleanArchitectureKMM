93e2c13d15204a06514236ff9531fb283ce47387
David <santamadavid@gmail.com>
2023-01-08 14:43:53 +0100

    Implemented NowPlaying image carousel on Android

--- androidApp/src/main/java/com/santukis/cleanarchitecturekmm/android/home/activities/HomeActivity.kt
+++ androidApp/src/main/java/com/santukis/cleanarchitecturekmm/android/home/activities/HomeActivity.kt
@@ -3,6 +3,7 @@ package com.santukis.cleanarchitecturekmm.android.home.activities
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
 import com.santukis.cleanarchitecturekmm.android.home.components.HomeScreen
 import com.santukis.cleanarchitecturekmm.android.theme.MovieTheme
 import com.santukis.injection.getDependencyInjector
@@ -16,6 +17,8 @@ class HomeActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
             MovieTheme {
                 HomeScreen(movieViewModel = viewModel)
