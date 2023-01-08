75b1f92d66fb1f0b88bd2bc91bc4f178c93fe5de
Santukis <santamadavid@gmail.com>
2023-01-08 14:45:12 +0100

    Implemented Now Playing image carousel on Ios

--- iosApp/iosApp/src/home/HomeScreen.swift
+++ iosApp/iosApp/src/home/HomeScreen.swift
@@ -16,15 +16,83 @@ struct HomeScreen: View {
     private var movieViewModel: MovieViewModel
     
     var body: some View {
-        VStack(alignment: .leading) {
-            let nowPlayingMovies = movieViewModel.state(
-                \.nowPlayingMoviesState,
-                equals: { state1, state2 in return state1 == state2 },
-                 mapper: { nowPlayingMoviesState in return nowPlayingMoviesState.movies }
+        let nowPlayingMovies = movieViewModel.state(
+            \.nowPlayingMoviesState,
+            equals: { state1, state2 in return state1 == state2 },
+            mapper: { nowPlayingMoviesState in return nowPlayingMoviesState }
+        )
+        GeometryReader { geometry in
+            HomeContent(
+                nowPlayingMoviesState: nowPlayingMovies,
+                geometry: geometry
+                
+            ).onAppear {
+                movieViewModel.loadNowPlayingMovies()
+            }
+        }.edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct HomeContent: View {
+    var nowPlayingMoviesState: MoviesState
+    var geometry: GeometryProxy
+    
+    var body: some View {
+        VStack {
+            NowPlayingContent(
+                movies: nowPlayingMoviesState.movies,
+                geometry: geometry
             )
-            
-        }.onAppear {
-            movieViewModel.loadNowPlayingMovies()
         }
+    }
+}
+
+struct NowPlayingContent: View {
+    var movies: [Movie]
+    var geometry: GeometryProxy
+    
+    @State private var selectedMovie: Int32 = 0
+    
+    var body: some View {
+            TabView(selection: $selectedMovie) {
+                ForEach(movies, id: \.self.id) { movie in
+                    ZStack() {
+                        AsyncImage(
+                            url: URL(string: movie.images.posterImage?.getUrl(size: .W_342()) ?? ""),
+                            content: { image in
+                                image.resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .overlay(
+                                        Rectangle()
+                                            .fill(
+                                                LinearGradient(
+                                                    gradient: Gradient(colors: [.clear, .black]),
+                                                    startPoint: .top,
+                                                    endPoint: .bottom
+                                                )
+                                            )
+                                        ,
+                                        alignment: .topTrailing
+                                    )
+                            },
+                            placeholder: {
+                                ProgressView()
+                                
+                            }
+                        )
+                        .onTapGesture {
+                            selectedMovie = movie.id
+                        }
+                        .frame(
+                            width: geometry.size.width
+                        )
+                    }
+                }
+            }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+            .frame(
+                width: geometry.size.width,
+                height: geometry.size.width
+            )
     }
 }
