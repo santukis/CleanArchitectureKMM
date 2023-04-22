d303893325445cdb00445af62b11cc16f7ac49d2
David <santamadavid@gmail.com>
2023-04-22 20:51:22 +0200

    ViewModels classes decoupled from ui classes

--- shared/injection/src/commonMain/kotlin/com/santukis/injection/DependencyInjector.kt
+++ shared/injection/src/commonMain/kotlin/com/santukis/injection/DependencyInjector.kt
@@ -2,10 +2,6 @@ package com.santukis.injection
 
 import com.santukis.viewmodels.movies.MovieDetailViewModel
 import com.santukis.viewmodels.movies.MoviesViewModel
-import org.kodein.di.DI
-import org.kodein.di.DIAware
-import org.kodein.di.direct
-import org.kodein.di.instance
 
 interface DependencyInjector {
 
@@ -15,12 +11,3 @@ interface DependencyInjector {
 }
 
 expect fun getDependencyInjector(moduleDependencies: Any? = null): DependencyInjector
-
-internal class KodeinDI(private var moduleDependencies: Any? = null): DIAware {
-
-    override val di: DI by DI.lazy {
-        import(applicationModule(moduleDependencies))
-    }
-}
-
-internal inline fun<reified Dependency> KodeinDI.getInstance(key: String): Dependency = di.direct.instance(key)
