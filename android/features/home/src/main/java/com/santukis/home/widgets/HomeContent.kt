7e6817a46d28ef830fc6575bf7091404e0e0b93a
Santukis <santamadavid@gmail.com>
2023-05-01 09:38:45 +0200

    HomeContent refactor

--- android/features/home/src/main/java/com/santukis/home/widgets/HomeContent.kt
+++ android/features/home/src/main/java/com/santukis/home/widgets/HomeContent.kt
@@ -3,10 +3,12 @@ package com.santukis.home.widgets
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.santukis.entities.movies.Movie
 import com.santukis.navigation.destination.DestinationArguments
 import com.santukis.viewmodels.core.entities.MovieSection
 import com.santukis.viewmodels.home.entities.HomeState
@@ -22,47 +24,72 @@ fun HomeContent(
             .background(Color.Black),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        homeState.nowPlayingMovies.takeIf { it.isNotEmpty() }?.let { movies ->
-            item {
-                NowPlayingContent(
-                    modifier = modifier,
-                    nowPlayingMovies = movies,
-                    navigateTo = navigateTo
-                )
-            }
-        }
+        addHighlight(
+            modifier = modifier,
+            movies = homeState.nowPlayingMovies,
+            navigateTo = navigateTo
+        )
 
-        homeState.upcomingMovies.takeIf { it.isNotEmpty() }?.let { movies ->
-            item {
-                MovieSectionContent(
-                    modifier = modifier,
-                    movies = movies,
-                    section = MovieSection.UpcomingMovies,
-                    navigateTo = navigateTo
-                )
-            }
-        }
+        addSection(
+            modifier = modifier,
+            movies = homeState.upcomingMovies,
+            section = MovieSection.UpcomingMovies,
+            navigateTo = navigateTo
+        )
 
-        homeState.popularMovies.takeIf { it.isNotEmpty() }?.let { movies ->
-            item {
-                MovieSectionContent(
-                    modifier = modifier,
-                    movies = movies,
-                    section = MovieSection.PopularMovies,
-                    navigateTo = navigateTo
-                )
-            }
-        }
+        addSection(
+            modifier = modifier,
+            movies = homeState.popularMovies,
+            section = MovieSection.PopularMovies,
+            navigateTo = navigateTo
+        )
 
-        homeState.couldLikeMovies.takeIf { it.isNotEmpty() }?.let { movies ->
-            item {
-                MovieSectionContent(
-                    modifier = modifier,
-                    movies = movies,
-                    section = MovieSection.CouldYouLikeMovies,
-                    navigateTo = navigateTo
-                )
-            }
+        addSection(
+            modifier = modifier,
+            movies = homeState.couldLikeMovies,
+            section = MovieSection.CouldYouLikeMovies,
+            navigateTo = navigateTo
+        )
+    }
+}
+
+private fun LazyListScope.addHighlight(
+    modifier: Modifier,
+    movies: List<Movie>,
+    navigateTo: (DestinationArguments) -> Unit
+) {
+    addItem(movies) {
+        NowPlayingContent(
+            modifier = modifier,
+            nowPlayingMovies = movies,
+            navigateTo = navigateTo
+        )
+    }
+}
+
+private fun LazyListScope.addSection(
+    modifier: Modifier,
+    movies: List<Movie>,
+    section: MovieSection,
+    navigateTo: (DestinationArguments) -> Unit
+) {
+    addItem(movies) {
+        MovieSectionContent(
+            modifier = modifier,
+            movies = movies,
+            section = section,
+            navigateTo = navigateTo
+        )
+    }
+}
+
+private fun LazyListScope.addItem(
+    movies: List<Movie>,
+    widget: @Composable () -> Unit
+) {
+    movies.takeIf { it.isNotEmpty() }?.let {
+        item {
+            widget()
         }
     }
 }
